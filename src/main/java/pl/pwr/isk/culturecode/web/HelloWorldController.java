2c7ce05515bf8ef75cf1ad8b27b05798ca68165b
zenerus1337 <cwirko12@gmail.com>
2024-05-05 20:48:12 +0200

    Adding simple RESTful API with GET

---
+++ src/main/java/pl/pwr/isk/culturecode/web/HelloWorldController.java
@@ -0,0 +1,14 @@
+package pl.pwr.isk.culturecode.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
