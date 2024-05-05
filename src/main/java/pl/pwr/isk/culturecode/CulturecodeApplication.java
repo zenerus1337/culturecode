eeb60b1ce64d9a93b49467d1844f13519040fd27
zenerus1337 <cwirko12@gmail.com>
2024-05-05 20:05:10 +0200

    skeleton from initializer for CultureCode

---
+++ src/main/java/pl/pwr/isk/culturecode/CulturecodeApplication.java
@@ -0,0 +1,13 @@
+package pl.pwr.isk.culturecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pwr/isk/culturecode/CulturecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pwr.isk.culturecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
